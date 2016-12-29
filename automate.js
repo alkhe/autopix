52096bdfbd49dd627e0df1b1723eb540e05b5631
edge <functaire@gmail.com>
2016-12-29 00:15:03 -0800

    use line drawing algorithm

--- automate.js
+++ automate.js
@@ -3,10 +3,16 @@ const robot = require('robotjs')
 const mouse = (...args) => robot.moveMouse(...args)
 const click = (...args) => robot.mouseClick(...args)
 const delay = (...args) => robot.setMouseDelay(...args)
+const down = () => robot.mouseToggle('down')
+const up = () => robot.mouseToggle('up')
+const drag = (...args) => robot.dragMouse(...args)
 
 module.exports = {
 	mouse,
 	click,
-	delay
+	delay,
+	down,
+	up,
+	drag
 }
 
--- index.js
+++ index.js
@@ -1,33 +1,87 @@
 const { resolve } = require('path')
 const { readdirSync: ls } = require('fs')
 const { contrast, map_image, read_png, write_image } = require('./image')
-const { mouse, click, delay } = require('./automate')
+const { mouse, click, delay, down, up, drag } = require('./automate')
 
-const ifile = resolve('./pictures', ls('./pictures')[0])
+const args = process.argv.slice(2)
+const file = resolve(args[0])
+const [ox = 150, oy = 150] = args.slice(1, 3).map(x => Number(x))
 
-read_png(ifile)
+function pen_draw(image) {
+	const { buf } = image
+
+	let iterations = 0
+
+	map_image(image, (addr, x, y) => {
+		if (buf[addr] === 0) {
+			mouse(ox + x, oy + y)
+			click()
+
+			if (iterations < 1000) {
+				delay(.5)
+			} else {
+				delay(500)
+				iterations = 0
+			}
+			iterations++
+		}
+	})
+}
+
+const up_state = { fill: false }
+const down_state = (left, y) => ({ fill: true, left, right: left, y })
+
+function line_groups({ buf, width, height }) {
+	const groups = []
+
+	let state = up_state
+
+	function commit() {
+		groups.push([state.left, state.right, state.y])
+		state = up_state
+	}
+
+	for (let y = 0; y < height; y++) {
+		for (let x = 0; x < width; x++) {
+			const offset = (width * y + x) << 2
+			const filled = buf[offset] === 0
+
+			if (state.fill) {
+				if (filled) {
+					state.right++
+				} else {
+					commit()
+				}
+			} else {
+				if (filled) {
+					state = down_state(x, y)
+				}
+			}
+		}
+
+		if (state.fill) {
+			commit()
+		}
+	}
+
+	return groups
+}
+
+function line_draw(image) {
+	const groups = line_groups(image)
+
+	groups.forEach(([l, r, y], i) => {
+		delay(i % 2000 === 0 ? 600 : 1)
+		const ry = oy + y
+		mouse(ox + l, ry)
+		down()
+		drag(ox + r, ry)
+		up()
+	})
+}
+
+read_png(file)
 	.then(contrast)
 	// .then(image => write_image(image, 'out.png'))
-	.then(image => {
-		const { buf } = image
-
-		const ox = 300
-		const oy = 300
-
-		let iterations = 0
-
-		map_image(image, (addr, x, y) => {
-			if (buf[addr] === 0) {
-				mouse(ox + x, oy + y)
-				click()
-
-				if (iterations < 1000) {
-					delay(.5)
-				} else {
-					delay(500)
-					iterations = 0
-				}
-				iterations++
-			}
-		})
-	})
+	// .then(pen_draw)
+	.then(line_draw)
